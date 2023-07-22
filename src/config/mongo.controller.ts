0a08e0c0948ab5660d9263b1971f75538b2e83ed
Tomasz Topola <t.topola.yt@gmail.com>
2023-07-22 16:23:17 +0200

    fix mongo controler

--- src/config/mongo.controller.ts
+++ src/config/mongo.controller.ts
@@ -2,19 +2,48 @@ import mongoose from 'mongoose'
 
 class MongoController{
 
+    private _connected: boolean = false
+
     async setupMongo(): Promise<void>{
         const mongoURL = process.env.MONGO_URL!
-        await mongoose.connect(mongoURL)
-        mongoose.connection.on('error', (err) => {
+        try {
+            await mongoose.connect(mongoURL)
+            mongoose.connection.on('error', (err) => {
+                this.close()
+                this.reattemptConnection()
+            })
+        } catch (err) {
             console.log('Error on DB Connection:')
             console.log(err)
-            process.exit(err.message)
-        })
+            this._connected = false
+            this.reattemptConnection()
+            return 
+        }
         console.log('Database connection established.')
     }
 
     async close(){
         mongoose.connection.close()
+    }
+    
+    private async reattemptConnection(){
+        while(!this._connected){
+            this._connected = true
+            const mongoURL = process.env.MONGO_URL!
+            try {
+                await mongoose.connect(mongoURL)
+            } catch (err) {
+                console.log('Error on DB Connection:')
+                console.log(err)
+                this._connected = false
+                console.log('No DB connection. Reattempting in 1 minutes...');
+            }
+            await this.delay(6000)
+        }
+    }
+
+    private delay(ms:number) {
+        return new Promise ( resolve => setTimeout(resolve, ms))
     }
 }
 
