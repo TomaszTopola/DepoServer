ddff5fe29f003fb4324638d2e0eb80d5e0ee0231
Tomasz Topola <t.topola.yt@gmail.com>
2023-07-22 20:57:58 +0200

    add json and urlencoded parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 
 import MongoController from './common/mongo.controller'
+import DepositRouter from './deposit/deposit.router'
 
 const app = express()
 dotenv.config({path: './.env'})
@@ -11,9 +12,15 @@ const main = async () => {
     console.log("connecting to Mongo...");
     await MongoController.setupMongo()
     
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+
+
     app.get('/', (req, res) =>{
         return res.status(200).send('Hello World')
     })
+
+    new DepositRouter(app).configureRoutes()
 
     app.listen(process.env.HTTP, () => console.log(`Listening on port ${process.env.HTTP}...`))
     
